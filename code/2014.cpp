5c9813493faf64660836ee4920aeaa0657a94a02
kreiitor <2955508016@qq.com>
2025-11-18 22:27:18 +0800

    routine

--- code/2014.cpp
+++ code/2014.cpp
@@ -1,6 +1,5 @@
 /*
 青年歌手大奖赛中，评委会给参赛选手打分。选手得分规则为去掉一个最高分和一个最低分，然后计算平均得分，请编程输出某选手的得分。
- 
 
 Input  输入
 输入数据有多组，每组占一行，每行的第一个数是n(2<n<=100)，表示评委的人数，然后是n个评委的打分。
@@ -15,11 +14,10 @@ Sample Output  示例输出
  
 */
 #include<iostream>
-#include<algorithm>
 #include<string>
 #include<vector>
 #include<sstream>
-#include<cmath>
+#include<iomanip>
 
 using namespace std;
 int main()
@@ -29,18 +27,18 @@ int main()
         //跳过空行
         if(line.empty()) continue;
         stringstream ss(line);
-        int n,m;
+        int n;
+        double m;
         ss >> n;
-        vector<double> scores(n);
+        double total=0.0;
+        double max=-11100.0,min=11100.0;
         while(ss>>m){
-            scores.push_back(m);
+            total += m;
+            if(m>max) max=m;
+            if(m<min) min=m;
         }
-        double m = 0.0;
-        for(double t : scores){
-            m += t;
-        }
-        m = (m-max(scores)-min(scores))/n
-        cout << m << endl;
+        double avg = (total-max-min)/(n-2);
+        cout <<fixed << setprecision(2) << avg << endl;
     }
     return 0;
 }
--- code/2015.cpp
+++ code/2015.cpp
@@ -1,16 +1,18 @@
 /*
-对于表达式n^2+n+41，当n在（x,y）范围内取整数值时（包括x,y）(-39<=x<y<=50)，判定该表达式的值是否都为素数。
-
+有一个长度为n(n<=100)的数列，该数列定义为从2开始的递增有序偶数，现在要求你按照顺序每m个数求出一个平均值，如果最后不足m个，则以实际数量求平均值。编程输出该平均值序列。
+1.特别烦人的边界问题，空格是否输出需要尽可能的讨论
+2.
 
 Input  输入
-输入数据有多组，每组占一行，由两个整数x，y组成，当x=0,y=0时，表示输入结束，该行不做处理。
+输入数据有多组，每组占一行，包含两个正整数n和m，n和m的含义如上所述。
 Output  输出
-对于每个给定范围内的取值，如果表达式的值都为素数，则输出"OK",否则请输出“Sorry”,每组输出占一行。
+对于每组输入数据，输出一个平均值序列，每组输出占一行。
 Sample Input  样例输入
-0 1
-0 0
+3 2
+4 2
 Sample Output  示例输出
-OK
+3 6
+3 7
 */
 
 #include<iostream>
@@ -20,7 +22,41 @@ OK
 using namespace std;
 
 int main()
-{
+{   
+    int n,m;
+    while(cin >> n >> m){
+        vector<int> evens;
+        int count=1,q = n/m;
+        int total=0;
+        if(q*m == n){
+            for(int i=1;i<=n;i++){
+                total += 2*i;
+                if(i%m==0){
+                    cout << total/m;
+                    total = 0;
+                    if(i<n) cout << ' ';
+                }
+            }
+
+
+        }
+        else{
+            int w = q*m;
+            for(int i=1;i<=w;i++){
+                total += 2*i;
+                if(i%m==0){
+                    cout << total/m;
+                    total = 0;
+                    cout << ' ';
+                }
+            }
+            for(int i=w+1;i<=n;i++){
+                total += 2*i;
+            }
+            cout << total/(n-w) << endl;
+        }
+        
+    }
     
     return 0;
 }
