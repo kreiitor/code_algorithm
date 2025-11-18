b321313d307c679eb4d0280e345689dd4ae52db0
kreiitor <2955508016@qq.com>
2025-11-18 17:41:48 +0800

    routine

--- code/2013.cpp
+++ code/2013.cpp
@@ -1,20 +1,32 @@
 /*
+当时的情况是这样的：
+第一天悟空吃掉桃子总数一半多一个，第二天又将剩下的桃子吃掉一半多一个，以后每天吃掉前一天剩下的一半多一个，到第n天准备吃的时候只剩下一个桃子。聪明的你，请帮悟空算一下，他第一天开始吃的时候桃子一共有多少个呢？
+1.现在是m个，明天就是m/2-1
+2.今天是n个，昨天就是2*(n+1)
 
+Input  输入
+输入数据有多组，每组占一行，包含一个正整数n（1<n<30），表示只剩下一个桃子的时候是在第n天发生的。
+Output  输出
+对于每组输入数据，输出第一天开始吃的时候桃子的总数，每个测试实例占一行。
+Sample Input  样例输入
+2
+4
+Sample Output  示例输出
+4
+22
 */
 #include<iostream>
-#include<string>
-#include<vector>
-#include<sstream>
-#include<unordered_set>
 
 using namespace std;
 int main()
 {
-    string line;
-    while(getline(cin,line)){
-        //跳过空行
-        if(line.empty()) continue;
-        stringstream ss(line);
+    int n;
+    while(cin >> n){
+        int m = 1;
+        for(int i=n;i>1;i--){
+            m = 2*(m+1);
+        }
+        cout << m << endl;
     }
     return 0;
 }
--- code/2014.cpp
+++ code/2014.cpp
@@ -1,11 +1,25 @@
 /*
+青年歌手大奖赛中，评委会给参赛选手打分。选手得分规则为去掉一个最高分和一个最低分，然后计算平均得分，请编程输出某选手的得分。
+ 
 
+Input  输入
+输入数据有多组，每组占一行，每行的第一个数是n(2<n<=100)，表示评委的人数，然后是n个评委的打分。
+Output  输出
+对于每组输入数据，输出选手的得分，结果保留2位小数，每组输出占一行。
+Sample Input  样例输入
+3 99 98 97
+4 100 99 98 97
+Sample Output  示例输出
+98.00
+98.50
+ 
 */
 #include<iostream>
+#include<algorithm>
 #include<string>
 #include<vector>
 #include<sstream>
-#include<unordered_set>
+#include<cmath>
 
 using namespace std;
 int main()
@@ -15,6 +29,18 @@ int main()
         //跳过空行
         if(line.empty()) continue;
         stringstream ss(line);
+        int n,m;
+        ss >> n;
+        vector<double> scores(n);
+        while(ss>>m){
+            scores.push_back(m);
+        }
+        double m = 0.0;
+        for(double t : scores){
+            m += t;
+        }
+        m = (m-max(scores)-min(scores))/n
+        cout << m << endl;
     }
     return 0;
 }
