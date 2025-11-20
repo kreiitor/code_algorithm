da8d501809e72ecdddaaffbeb39444e81ca4b4eb
kreiitor <2955508016@qq.com>
2025-11-20 14:17:49 +0800

    routine

---
+++ hdoj2021-2040/2027.cpp
@@ -0,0 +1,20 @@
+/*
+
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<sstream>
+#include<unordered_set>
+
+using namespace std;
+int main()
+{
+    string line;
+    while(getline(cin,line)){
+        //跳过空行
+        if(line.empty()) continue;
+        stringstream ss(line);
+    }
+    return 0;
+}
