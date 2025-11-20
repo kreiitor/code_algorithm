b3c41f9edc66c25cd09cf15d63364689c267ccd5
kreiitor <2955508016@qq.com>
2025-11-20 18:53:42 +0800

    routine

--- hdoj2021-2040/2021.cpp
+++ hdoj2021-2040/2021.cpp
@@ -1,20 +1,38 @@
 /*
+如果每个老师的工资额都知道，最少需要准备多少张人民币，才能在给每位老师发工资的时候都不用老师找零呢？
+这里假设老师的工资都是正整数，单位元，人民币一共有100元、50元、10元、5元、2元和1元六种。
+1.经典的贪心算法
 
+Input  输入
+输入数据包含多个测试实例，每个测试实例的第一行是一个整数n（n<100），表示老师的人数，然后是n个老师的工资。
+n=0表示输入的结束，不做处理。
+Output  输出
+对于每个测试实例输出一个整数x,表示至少需要准备的人民币张数。每个输出占一行。
+Sample Input  样例输入
+3
+1 2 3
+0
+Sample Output  示例输出
+4
 */
 #include<iostream>
-#include<string>
-#include<vector>
-#include<sstream>
-#include<unordered_set>
 
 using namespace std;
 int main()
 {
-    string line;
-    while(getline(cin,line)){
-        //跳过空行
-        if(line.empty()) continue;
-        stringstream ss(line);
+    int money[] = {100,50,10,5,2,1};
+    int n;
+    while(cin>>n){
+        if(n==0) break;
+        int salary,count=0;
+        for(int i=0;i<n && cin>>salary;i++){
+            for(int j=0;j<6;j++){
+                count += salary/money[j];
+                salary -= salary/money[j]*money[j];
+            }
+        }
+        cout << count << endl;
+        
     }
     return 0;
 }
--- hdoj2021-2040/2022.cpp
+++ hdoj2021-2040/2022.cpp
@@ -1,20 +1,44 @@
 /*
+分数送上来了，是我做决定的时候了，我的一个选拔原则是，要选一个面试分数绝对值（必须还是32位整数）最大的MM。
+特别说明：如果不幸选中一个负分的MM,也没关系，因为我觉得，如果不能吸引你，那要想法恶心你。
+ 
+
+Input  输入
+输入数据有多组，每组的第一行是两个整数m和n，表示应聘MM的总共的行列数，然后是m行整数，每行有n个，m和n的定义见题目的描述。
+Output  输出
+对于每组输入数据，输出三个整数x,y和s，分别表示选中的MM的行号、列号和分数。
+note:行号和列号从一开始，如果有多个MM的分数绝对值一样，那么输出排在最前面的一个（即行号最小的那个，如果行号相同则取列号最小的那个）。
+Sample Input  样例输入
+2 3
+1 4 -3
+-7 3 0
+Sample Output  示例输出
+2 1 -7
 
 */
 #include<iostream>
-#include<string>
 #include<vector>
-#include<sstream>
-#include<unordered_set>
+#include<algorithm>
 
 using namespace std;
 int main()
 {
-    string line;
-    while(getline(cin,line)){
-        //跳过空行
-        if(line.empty()) continue;
-        stringstream ss(line);
+    int m,n;
+    while(cin >> m >> n){
+        int score;
+        int row_index,col_index,max_abs_score;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n && cin>>score;j++){
+                if(i==1&&j==1){
+                    row_index=1;col_index=1;max_abs_score=score;
+                    continue;
+                }
+                if(abs(score)>abs(max_abs_score)){
+                    row_index = i;col_index=j;max_abs_score=score;
+                }
+            }
+        }
+        cout << row_index << ' ' << col_index << ' ' << max_abs_score << endl;
     }
     return 0;
 }
