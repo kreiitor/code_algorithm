8ab4cea6d7f08e14c6d70d27bfe20584e7ecf10f
kreiitor <2955508016@qq.com>
2025-11-24 13:55:46 +0800

    routine

--- hdoj2021-2040/2039.cpp
+++ hdoj2021-2040/2039.cpp
@@ -1,36 +1,33 @@
 /*
-A和B 都是由3个整数组成，分别表示时分秒，比如，假设A为34 45 56，就表示A所表示的时间是34小时 45分钟 56秒。
- 
+给定三条边，请你判断一下能不能组成一个三角形。
+1.三个正数判断只用判断两边之和大于第三遍。
+2.正数double正整数int
+
 Input  输入
-输入数据有多行组成，首先是一个整数N，表示测试实例的个数，然后是N行数据，每行有6个整数AH,AM,AS,BH,BM,BS，分别表示时间A和B所对应的时分秒。题目保证所有的数据合法。
+输入数据第一行包含一个数M，接下有M行，每行一个实例，包含三个正数A,B,C。其中A,B,C <1000;
 Output  输出
-对于每个测试实例，输出A+B，每个输出结果也是由时分秒3部分组成，同时也要满足时间的规则（即：分和秒的取值范围在0~59），每个输出占一行，并且所有的部分都可以用32位整数表示。
+对于每个测试实例，如果三条边长A,B,C能组成三角形的话，输出YES，否则NO。
 Sample Input  样本输入
 2
-1 2 3 4 5 6
-34 45 56 12 23 34
+1 2 3
+2 2 2
 Sample Output  样本输出
-5 7 9
-47 9 30
+NO
+YES
 */
-// 时间相加（第二题）—— 老铁防寄进阶版
 #include <iostream>
+#include<cmath>
 using namespace std;
 
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int ah, am, as, bh, bm, bs;
-        cin >> ah >> am >> as >> bh >> bm >> bs;
-        
-        int tot_s = as + bs;
-        int tot_m = am + bm + tot_s / 60;
-        int tot_h = ah + bh + tot_m / 60;
-        
-        cout << tot_h << " "
-             << (tot_m % 60) << " "
-             << (tot_s % 60) << endl;
+int main() 
+{
+    int m;
+    cin >> m;
+    double a,b,c;
+    for(int i=0;i<m &&cin>>a>>b>>c;++i){
+        bool is_triangle=(a+b>c)&&(a+c>b)&&(b+c>a);
+        cout << (is_triangle?"YES":"NO") << endl;
     }
+
     return 0;
 }
--- hdoj2021-2040/2040.cpp
+++ hdoj2021-2040/2040.cpp
@@ -1,36 +1,44 @@
 /*
-A和B 都是由3个整数组成，分别表示时分秒，比如，假设A为34 45 56，就表示A所表示的时间是34小时 45分钟 56秒。
- 
+古希腊数学家毕达哥拉斯在自然数研究中发现，220的所有真约数(即不是自身的约数)之和为：
+1+2+4+5+10+11+20+22+44+55+110＝284。
+而284的所有真约数为1、2、4、71、 142，加起来恰好为220。人们对这样的数感到很惊奇，并称之为亲和数。
+一般地讲，如果两个数中任何一个数都是另一个数的真约数之和，则这两个数就是亲和数。
+你的任务就编写一个程序，判断给定的两个数是否是亲和数
+1.累加真约数即可
+
 Input  输入
-输入数据有多行组成，首先是一个整数N，表示测试实例的个数，然后是N行数据，每行有6个整数AH,AM,AS,BH,BM,BS，分别表示时间A和B所对应的时分秒。题目保证所有的数据合法。
+输入数据第一行包含一个数M，接下有M行，每行一个实例,包含两个整数A,B； 其中 0 <= A,B <= 600000 ;
 Output  输出
-对于每个测试实例，输出A+B，每个输出结果也是由时分秒3部分组成，同时也要满足时间的规则（即：分和秒的取值范围在0~59），每个输出占一行，并且所有的部分都可以用32位整数表示。
+对于每个测试实例，如果A和B是亲和数的话输出YES，否则输出NO。
 Sample Input  样本输入
 2
-1 2 3 4 5 6
-34 45 56 12 23 34
+220 284
+100 200
 Sample Output  样本输出
-5 7 9
-47 9 30
+YES
+NO
 */
-// 时间相加（第二题）—— 老铁防寄进阶版
 #include <iostream>
 using namespace std;
 
+int func(int n){
+    int sum=0;
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0) sum = (i*i==n)?(sum+i):(sum+i+n/i);
+    }
+    return sum-n;
+}
+
 int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int ah, am, as, bh, bm, bs;
-        cin >> ah >> am >> as >> bh >> bm >> bs;
-        
-        int tot_s = as + bs;
-        int tot_m = am + bm + tot_s / 60;
-        int tot_h = ah + bh + tot_m / 60;
-        
-        cout << tot_h << " "
-             << (tot_m % 60) << " "
-             << (tot_s % 60) << endl;
+    int m;cin>>m;
+    int a,b;
+    for(int i=0;i<m && cin>>a>>b;++i){
+        bool flag;
+        if(a==0||b==0) flag = false;
+        int a_,b_;//记录两个数的各自真约数之和；
+        a_ = func(a);b_=func(b);
+        flag = (a_==b && b_==a);
+        cout << (flag?"YES":"NO") << endl;
     }
     return 0;
 }
